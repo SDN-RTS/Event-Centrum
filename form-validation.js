0790bf851c97a41d4db5d4f6c559a0a403d84da4
Thor Würtzner <6273158@rts-365.dk>
2020-09-03 09:46:39 +0200

    validation done

--- form-validation.js
+++ form-validation.js
@@ -1,13 +1,22 @@
 
 function formValidator(form) {
     document.querySelector('#submit').addEventListener('click', function(event) {
-        event.preventDefault();
+        event.preventDefault()
         let inputs = form.querySelectorAll('input');
+        let textArea = form.querySelector('.footerTextarea')
+
         for (let i = 0; i < inputs.length; i++) {
             let input = inputs[i];
             if (input.value == "") {
                 input.classList.add('formError');
+            } else {
+                input.classList.remove('formError');
             }
+        } 
+        if (textArea.value == "") {
+            textArea.classList.add('formError');
+        } else {
+            textArea.classList.remove('formError');
         }
     })
 }
